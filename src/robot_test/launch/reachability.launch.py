b4aa728867ccd2831b139f8c1371eec0ed83dc6a
haolin-io <haolin.dev@proton.me>
2026-09-25 03:27:24 +0800

    Switched to MoveIt to test reachability.

--- src/robot_test/launch/reachability.launch.py
+++ src/robot_test/launch/reachability.launch.py
@@ -1,17 +1,41 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description() -> LaunchDescription:
+    ur_type = LaunchConfiguration("ur_type")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+
+    moveit = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare("ur_moveit_config"), "launch", "ur_moveit.launch.py"]
+            )
+        ),
+        launch_arguments={
+            "ur_type": ur_type,
+            "use_sim_time": use_sim_time,
+            "launch_rviz": launch_rviz,
+            "launch_servo": "false",
+        }.items(),
+    )
+    go_to_target = Node(
+        package="robot_test",
+        executable="reachability_node",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
     return LaunchDescription(
         [
-            Node(
-                package="robot_test",
-                executable="reachability_node",
-                output="screen",
-                parameters=[
-                    {"cartesian_step_m": 0.01},
-                ],
-            ),
+            DeclareLaunchArgument("ur_type", default_value="ur16e"),
+            DeclareLaunchArgument("use_sim_time", default_value="true"),
+            DeclareLaunchArgument("launch_rviz", default_value="true"),
+            moveit,
+            go_to_target,
         ]
     )
